75b16e2878e8663ccc0c1cbbe9c2a10939a86ae5
Hieu Don <dont1@mcmaster.ca>
2021-03-12 13:58:06 -0500

    Add Hello World Program to BMS for members to work on

---
+++ BMS_Modules/BMS/SW4STM32/STM32H743ZI_Nucleo/Debug/Drivers/BSP/STM32H7xx_Nucleo/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Users/hieud/Documents/stm32-mcu/BMS_Modules/BMS/Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.c 
+
+OBJS += \
+./Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.o 
+
+C_DEPS += \
+./Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.o: C:/Users/hieud/Documents/stm32-mcu/BMS_Modules/BMS/Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.c Drivers/BSP/STM32H7xx_Nucleo/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32H743xx -DUSE_STM32H7XX_NUCLEO_144_MB1364 -c -I../../../Inc -I../../../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../../../Drivers/STM32H7xx_HAL_Driver/Inc -I../../../Drivers/BSP/STM32H7xx_Nucleo -I../../../Utilities/Fonts -I../../../Utilities/CPU -I../../../Drivers/CMSIS/Include -Os -ffunction-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.d" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
---
+++ BMS_Modules/BMS/SW4STM32/STM32H743ZI_Nucleo/Debug/Drivers/CMSIS/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Users/hieud/Documents/stm32-mcu/BMS_Modules/BMS/Src/system_stm32h7xx.c 
+
+OBJS += \
+./Drivers/CMSIS/system_stm32h7xx.o 
+
+C_DEPS += \
+./Drivers/CMSIS/system_stm32h7xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/CMSIS/system_stm32h7xx.o: C:/Users/hieud/Documents/stm32-mcu/BMS_Modules/BMS/Src/system_stm32h7xx.c Drivers/CMSIS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32H743xx -DUSE_STM32H7XX_NUCLEO_144_MB1364 -c -I../../../Inc -I../../../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../../../Drivers/STM32H7xx_HAL_Driver/Inc -I../../../Drivers/BSP/STM32H7xx_Nucleo -I../../../Utilities/Fonts -I../../../Utilities/CPU -I../../../Drivers/CMSIS/Include -Os -ffunction-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/CMSIS/system_stm32h7xx.d" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
