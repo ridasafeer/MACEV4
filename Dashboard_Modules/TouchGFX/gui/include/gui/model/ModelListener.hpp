2c3c9d5aa232051f65539de783d2e583e865c9c1
Liam Telenko <telenkol@mcmaster.ca>
2022-03-30 04:40:44 +0000

    refactor: Remove unused classes and methods from containers

---
+++ Dashboard_Modules/TouchGFX/gui/include/gui/model/ModelListener.hpp
@@ -0,0 +1,37 @@
+/*
+ * File: ModelListener.hpp
+ *
+ * Summary: This file defines the functions that the screen presenters can use to interact with the data
+ * 			provided by the TouchGFX model. Functions should be declared as empty and overridden by each
+ * 			presenter that needs to use them.
+ */
+
+#ifndef MODELLISTENER_HPP
+#define MODELLISTENER_HPP
+
+// Imports
+#include <gui/model/Model.hpp>
+
+class ModelListener
+{
+public:
+    ModelListener() : model(0) {}
+    
+    virtual ~ModelListener() {}
+
+    void bind(Model* m)
+    {
+        model = m;
+    }
+
+
+    /*
+     * Functions to notify presenters of updated data from the model.
+     * Need to be overridden by the presenter implementations that require it.
+     */
+    virtual void batteryTempUpdated(int temp){}
+protected:
+    Model* model;
+};
+
+#endif // MODELLISTENER_HPP
---
+++ Dashboard_Modules/TouchGFX/gui/src/model/Model.cpp
@@ -0,0 +1,35 @@
+/*
+ * File: Model.cpp
+ *
+ * Summary: This file is the model for the TouchGFX GUI interface. The declaration of this
+ * 			class can be found in the gui/include/model/Model.hpp file.
+ *
+ * 			TouchGFX/STM32 uses a MVP (model, view, presenter) design, the supporting files for the views and presenters
+ * 			can be found in the gui/src folder.
+ *
+ * 			The model file interacts directly with the backend communication channels and controls the data processing
+ * 			for the GUI. The listener functions used in this file can be found in ModelListener.hpp, which declares the
+ * 			functions to be implemented by the presenters if they want to handle each particular dataset sent by the model.
+ */
+
+// Imports
+#include <gui/model/Model.hpp>
+#include <gui/model/ModelListener.hpp>
+
+// Temporary variable for testing
+int count_model = 0;
+
+Model::Model() : modelListener(0) {
+
+}
+
+void Model::tick() {
+
+	// Temporary - for testing only
+	// Increment count if less than 100, reset to 0 when >= 100
+	count_model = count_model < 100 ? count_model + 1 : 0;
+
+	// On each tick, update the battery temperature with the count
+	modelListener->batteryTempUpdated(count_model);
+
+}
