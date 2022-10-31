e5d423c3ba8cb6ce05e4c70740bff1c834aa6ba7
forrest-herman <forrestherman13@gmail.com>
2022-10-30 21:14:09 -0400

    work in progress on touchGFX and Dashboard_modules

---
+++ Dashboard_Modules/STM32CubeIDE/Debug/Application/User/generated/image_horiz.d
@@ -0,0 +1,4 @@
+Application/User/generated/image_horiz.o: \
+ D:/ProgramFiles/STM32_workspace/stm32-mcu/Dashboard_Modules/TouchGFX/generated/images/src/image_horiz.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
---
+++ Dashboard_Modules/STM32CubeIDE/Debug/Application/User/generated/image_top_grabber.d
@@ -0,0 +1,4 @@
+Application/User/generated/image_top_grabber.o: \
+ D:/ProgramFiles/STM32_workspace/stm32-mcu/Dashboard_Modules/TouchGFX/generated/images/src/image_top_grabber.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
